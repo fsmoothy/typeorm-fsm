b8a050815b9ccbe100d7da2137117503a961f341
bondiano <batbondik0@gmail.com>
2023-09-29 19:28:37 +0600

    fix: add missed reexports

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 export {
   StateMachine,
+  StateMachineParameters,
   t,
   IStateMachine,
   Transition,
   isStateMachineError,
+  All,
 } from 'fsmoothy';
 export { StateMachineEntity } from './fsm.entity';
 export { state } from './state';
